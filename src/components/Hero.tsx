273509d8f924dcad90919a35b849697b1a7e5fe4
Zahra <bghzahra@outlook.com>
2022-03-22 22:55:53 +0000

    finished her component with css and images

---
+++ src/components/Hero.tsx
@@ -0,0 +1,9 @@
+export default function Hero():JSX.Element{
+    return(
+        <div className="hero-content">
+            <img src="images/Group77.png" alt="people laughing" className="hero-img"/>
+            <h1>Online Experiences</h1>
+            <p>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
+        </div>
+    )
+}
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,9 +1,11 @@
 import NavBar from "./NavBar"
+import Hero from "./Hero"
 
 export default function Main():JSX.Element{
     return(
         <>
         <NavBar/>
+        <Hero/>
         </>
     )
 }
